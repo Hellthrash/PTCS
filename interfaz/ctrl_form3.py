78728a15aa3b64469e7c5916e97c0ce22129d8b1
Patocho <patovaldivia4@gmail.com>
2015-09-01 17:15:51 -0300

    arreglando repo para subir version final

--- interfaz/ctrl_form3.py
+++ interfaz/ctrl_form3.py
@@ -7,19 +7,28 @@ import model
 
 class FormCitas(QtGui.QDialog):
 
-    def __init__(self, parent=None, paciente_rut=None, medico_rut = None,
-        fecha = None, sintomas = None, diagnostico = None, recomendaciones = None,
-        receta = None):
+    def __init__(self, parent=None, paciente_rut=None, medico_rut=None,
+        fecha=None, sintomas=None, diagnostico=None, recomendaciones=None,
+        receta=None):
         super(FormCitas, self).__init__(parent)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-        if paciente_rut is None:# Cuando no recibe rut crea, cuando recibe edita
+        if paciente_rut is None:  # Cuando no recibe rut crea, cuando recibe edita
             self.ui.save.clicked.connect(self.crear_ct)
         else:
-            self.colocar_datos(paciente_rut,medico_rut,fecha,sintomas,diagnostico,recomendaciones,receta)
+            self.colocar_datos(paciente_rut, medico_rut, fecha, sintomas,
+                diagnostico, recomendaciones, receta)
             self.ui.save.clicked.connect(self.editar_cita)
+            self.ui.cancel.clicked.connect(self.exitform)
 
-    def colocar_datos(self, paciente_rut,medico_rut,fecha,sintomas,diagnostico,recomendaciones,receta):
+    def exitform(self):
+        from ctrl_grid import *
+        self.close()
+        self.rld = Vtn3()
+        self.rld.reloadG()
+
+    def colocar_datos(self, paciente_rut, medico_rut, fecha, sintomas,
+        diagnostico, recomendaciones, receta):
         #ingresa los datos de los medicos en las grillas
 
         self.ui.p_rut.setText(str(paciente_rut))
@@ -42,7 +51,7 @@ class FormCitas(QtGui.QDialog):
         return (paciente_rut,medico_rut,fecha,sintomas,diagnostico,recomendaciones,receta)
 
     def crear_ct(self):
-        paciente_rut,medico_rut,fecha,sintomas,diagnostico,recomendaciones,receta = self.obtener_datos()
+        paciente_rut, medico_rut, fecha, sintomas, diagnostico, recomendaciones,receta = self.obtener_datos()
         print(paciente_rut)
         print(medico_rut)
         print(fecha)
